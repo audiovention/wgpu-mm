b5254396b51df5d8e0c54481edde9640adf37586
FL33TW00D <fleetwoodpersonal@gmail.com>
2023-10-09 10:13:30 +0900

    chore: working

--- src/qgemv.rs
+++ src/qgemv.rs
@@ -16,7 +16,31 @@ const N: usize = 51868;
 const K: usize = 384;
 pub const ABSMAX: f32 = 0.2; //Data ranges from -10 to 10, divide by 50
 
-pub fn qgemv_1(tera: &mut Tera, context: &mut Context) -> (Workload, String) {
+pub fn sint8_gemv_1(tera: &mut Tera, context: &mut Context) -> (Workload, String) {
+    tera.add_raw_template("qgemv_1.wgsl", include_str!("../shaders/gemv/qgemv_1.wgsl"))
+        .unwrap();
+    let workgroup_size_x = 8;
+    let workgroup_size_y = 8;
+    let workgroup_size_z = 1;
+    let colPerThread = 1;
+
+    let wgs = WorkgroupSize(workgroup_size_x as _, workgroup_size_y, workgroup_size_z);
+
+    let requiredGroups = Workload::ceil(N / (colPerThread * 4), wgs.total() as usize);
+
+    let workload = Workload::new(WorkgroupCount(requiredGroups as _, 1, 1), wgs);
+    println!("Workload: {:?}", workload);
+    context.insert("colPerThread", &colPerThread);
+    context.insert("workgroup_size_x", &workload.size().0);
+    context.insert("workgroup_size_y", &workload.size().1);
+    context.insert("workgroup_size_z", &workload.size().2);
+    context.insert("scale", &ABSMAX);
+    let shader = tera.render("qgemv_1.wgsl", context).unwrap();
+    println!("Shader: {}", shader);
+    (workload, shader)
+}
+
+pub fn sint4_gemv_1(tera: &mut Tera, context: &mut Context) -> (Workload, String) {
     tera.add_raw_template("qgemv_1.wgsl", include_str!("../shaders/gemv/qgemv_1.wgsl"))
         .unwrap();
     let workgroup_size_x = 8;
@@ -53,7 +77,7 @@ mod tests {
         let mut tera = tera::Tera::default();
         let mut context = tera::Context::new();
         let dims = insert_matrix_dims(&mut context);
-        let (workload, shader) = qgemv_1(&mut tera, &mut context);
+        let (workload, shader) = sint8_gemv_1(&mut tera, &mut context);
         test_harness(workload, shader, dims, Quantization::SInt8).await;
     }
 }
